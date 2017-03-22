0d8967b5669432d16d88f9991d508b14137f6879
Ewere Diagboya <nimboya@gmail.com>
2017-03-22 19:39:36 +0100

    profile update module

--- changepassword.php
+++ changepassword.php
@@ -1,6 +1,6 @@
 <?php
 $curuser = "";
-$qry = mysql_real_escape_string("select * from members where username = $curuser");
+$qry = mysql_real_escape_string("select * from members where username = '$curuser'");
 $runqry = mysql_query($qry);
 $user = mysql_fetch_array($runqry);
 $oldpassword = $user['oldpassword'];
@@ -8,18 +8,16 @@ $typedpassword = strip_tags($_POST['typedpassword']);
 $newpassword = strip_tags($_POST['newpassword']);
 $retypepassword = strip_tags($_POST['retypepassword']);
 
-if($oldpassword == $typedpassword) {
-    // Change Password
+if($oldpassword != $typedpassword) {
+    $response = "Old Password does not match";
+}
+else if($newpassword != $retypepassword) {
+    $response = "New Password dont match";
+} 
+else {
+     // Change Password
     $updateq = strip_tags("UPDATE member SET password = '$newpassword' WHERE username='$curuser'");
     $runq = mysql_query($updateq);
-    $response = "Password Changed Successfully";   
-}
-
-else if($newpassword == $retypepassword) {
-    $response = "The Password you typed dont match, please try again";
-} 
-
-}else {
-    $response = "Old Password is no valid, Password Not Changed";
+    $response = "Password Changed Successfully"; 
 }
 ?>
---
+++ editprofile.php
@@ -0,0 +1,17 @@
+<?php
+$curuser = "";
+$runqry = mysql_query("select id from members where username = '$curuser'");
+$u = mysql_fetch_array($runqry);
+
+$userid = 
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$gender = $_POST['gender'];
+
+// Perform Update
+$updqry = "UPDATE members SET firstname = '$firstname', lastname='$lastname', 
+          gender='$gender' WHERE id=$userid";
+
+$runqry = mysql_query($updqry);
+$response = "Profile Updated Successfully";
+?>
