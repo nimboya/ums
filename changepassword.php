377a6254cee4c7a879792dfe7aa9fa729b370c23
Ewere Diagboya <nimboya@gmail.com>
2017-03-22 16:50:04 +0100

    change password

---
+++ changepassword.php
@@ -0,0 +1,16 @@
+<?php
+$curuser = "";
+$qry = mysql_real_escape_string("select * from members where username = $curuser");
+$oldpassword = mysql_query();
+$typedpassword = strip_tags($_POST['typedpassword']); 
+$newpassword = strip_tags($_POST['newpassword']);
+
+if($oldpassword == $typedpassword) {
+    // Change Password
+    $updateq = strip_tags("UPDATE member SET password = '$newpassword' WHERE username='$curuser'");
+    $runq = mysql_query($updateq);
+    $response = "Password Changed Successfully";   
+} else {
+    $response = "Old Password is no valid, Password Not Changed";
+}
+?>
